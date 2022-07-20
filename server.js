d20a9b767fdc06d24b0b3935ae04b8753d062d53
Raman Mishra <91136101+raman0451@users.noreply.github.com>
2022-07-20 14:52:47 +0530

    changed app.js to server.js due to error in build of jenkins

---
+++ server.js
@@ -0,0 +1,24 @@
+const express= require('express')
+const mongoose= require('mongoose')
+
+const app= express()  //start express
+
+const url= 'mongodb://localhost/UsersDb'
+
+mongoose.connect(url, {useNewUrlParser: true}) //useNewUrlParser is to avoid some warnings
+
+const con= mongoose.connection
+
+con.on('open', function(){
+    console.log('Database connected...')
+})    //check connection on nodemon
+
+app.use(express.json())    //to tell the app that user will enter data in JSON format
+
+const userRouter= require('./routes/user/routes') 
+app.use('/user', userRouter)
+
+
+app.listen(4000, function(){
+    console.log('Server Started on port 4000')
+})
